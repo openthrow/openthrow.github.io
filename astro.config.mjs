c11798a263151f59dae2302e81fe41c33903669e
Scott Nesham <sanesham@gmail.com>
2026-01-11 07:53:40 -0600

    update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://openthrow.github.io',
+	base: '/docs',
+	outDir: './dist',
 	integrations: [
 		starlight({
 			title: 'Open Throw',
