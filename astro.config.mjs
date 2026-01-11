37201c13c06d9a469d3dd25b96a4f0b17472f7e7
Scott Nesham <sanesham@gmail.com>
2026-01-11 08:16:39 -0600

    update to use the TLD

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://openthrow.github.io',
+	site: 'https://openthrow.com',
 	base: '/',
 	outDir: './dist',
 	integrations: [
